c7194c6e4e437a24a6239416dc4c1e6e370902c7
GJHeyes <gregory-heyes@hotmail.com>
2022-10-28 16:57:49 +0100

    added attack and graph to project

--- assignCardsButton.js
+++ assignCardsButton.js
@@ -1,9 +1,11 @@
+const chart = document.getElementById("Bar")
 const assignCardsButton = document.querySelector("#cardButton")
 assignCardsButton.addEventListener('click', (event)=>{
     event.preventDefault()
     assignCardsButton.disable = true;
     if(assignCardsButton.innerText !== "Loading..."){
         assignCardsButton.disable = false;
+        chart.classList.remove("hidden")
         if(notClicked){
             assignCardsButton.innerText = "Reset?"
             notClicked = false
--- isFlipped.js
+++ isFlipped.js
@@ -1,7 +1,10 @@
 document.addEventListener('click', (e) =>{ //e for event
     let cardClassList = e.target.parentNode.classList
     if(cardClassList.length > 0 && cardClassList[1].includes('cpu')) {
-        cardClassList.toggle('is-not-flipped')
-        cardClassList.toggle('is-flipped')
+        if(flip === false && playerSelect.length !=0){
+            cardClassList.toggle('is-not-flipped')
+            cardClassList.toggle('is-flipped')
+            flip = true
+        }
     }
 })
