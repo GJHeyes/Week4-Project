ee2de560fc719fd60d36f9360b58204b057c261f
GJHeyes <gregory-heyes@hotmail.com>
2022-10-28 09:43:04 +0100

    Changed so card.id is passed to the div not the img

--- createCard.js
+++ createCard.js
@@ -3,7 +3,7 @@ function createcard(card){
     const top = document.createElement("img")
     const bottom = document.createElement("img")
     //let type = card.types[0] type of pokemon
-    top.setAttribute("id", card.id)
+    div.setAttribute("id", card.id)
     //nametag.innerText = card.name
     if(stackingComputerDeck){
         top.src = "https://iili.io/bdmwPt.png" //../src/pokemonCard.png 
--- isFlipped.js
+++ isFlipped.js
@@ -1,4 +1,6 @@
 document.addEventListener('click', (e) =>{ //e for event
     let cardClassList = e.target.parentNode.classList
-    if(cardClassList.length > 0 && cardClassList[1].includes('computer')) cardClassList.toggle('is-flipped')
+    if(cardClassList.length > 0 && cardClassList[1].includes('computer')) {
+        cardClassList.toggle('is-flipped')
+    }
 })
