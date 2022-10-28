d5b2c90c8d1ed886c27c36739ac254aa7630357c
GJHeyes <gregory-heyes@hotmail.com>
2022-10-28 13:18:49 +0100

    fixed flip bug with anything with computer in the class list

--- isFlipped.js
+++ isFlipped.js
@@ -1,6 +1,6 @@
 document.addEventListener('click', (e) =>{ //e for event
     let cardClassList = e.target.parentNode.classList
-    if(cardClassList.length > 0 && cardClassList[1].includes('computer')) {
+    if(cardClassList.length > 0 && cardClassList[1].includes('cpu')) {
         cardClassList.toggle('is-not-flipped')
         cardClassList.toggle('is-flipped')
     }
