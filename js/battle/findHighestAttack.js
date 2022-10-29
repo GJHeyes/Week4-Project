39bcee73b2628dc36842565e7f18148421e2b85c
GJHeyes <gregory-heyes@hotmail.com>
2022-10-29 14:12:46 +0100

    Updated so all cards have two attacks with damage

--- js/battle/findHighestAttack.js
+++ js/battle/findHighestAttack.js
@@ -15,7 +15,7 @@ function findHighestAttack(card){
     if(card.hasOwnProperty("attacks")){
         for(let i = 0; i<3;i++){
             if(card.attacks.length >i){
-                if(typeof card.attacks[i] != undefined){
+                if(typeof card.attacks[i] !== undefined){
                     cardAttacks.data[i].error = card.attacks[i].damage
                     cardAttacks.data[i].error = cardAttacks.data[i].error.replace("×", "").replace("+", "").replace("-", "")
                     cardAttacks.data[i].attack = Number(cardAttacks.data[i].error)
